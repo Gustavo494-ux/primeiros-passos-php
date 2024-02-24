0714660f253c37fffb7fd668114dc800b492303d
Gustavo Gama <gustavogama494@gmail.com>
2024-02-24 17:20:36 -0300

    modulo 1 do curso avancando com php

---
+++ src/avancando/listas.php
@@ -0,0 +1,8 @@
+<?php
+
+$idadeList =  [18,19,21,23,44];
+
+
+$umaIdade = $idadeList[3];
+
+echo $umaIdade.PHP_EOL;
---
+++ src/avancando/loop-lista.php
@@ -0,0 +1,8 @@
+<?php
+
+$idadeList =  [18,19,21,23,44];
+
+foreach ($idadeList as $key => $value) {
+    echo $value.PHP_EOL;
+}
+
