bb93c1c04ce7a9ec74552e17efdae6abec6653ba
Gustavo Gama <gustavogama494@gmail.com>
2024-02-24 09:33:30 -0300

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+// Exibe a data e hora atuais
+echo "Data e Hora Atuais: " . date("Y-m-d H:i:s") . "<br>";
+
+// Exibe uma mensagem de boas-vindas
+echo "Bem-vindo ao PHP! Este é um exemplo simples de script PHP. Gustavo";
+
